2173b28c11539f052fa899edc222cfbe342c6f58
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-07-11 10:21:22 +0530

    Add files via upload

---
+++ Palliandrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Palliandrome {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num=sc.nextInt();
+		int copy= num;
+		int rev=0,rem;
+		while(num>0) {
+			rem = num % 10;
+			rev = rev * 10 + rem;
+			num = num / 10;
+		}
+		if (rev == copy) {
+			System.out.println("Palliandrome");
+		}else {
+			System.out.println("Not Paliiandrome");
+		}
+		sc.close();
+	}
+
+}
