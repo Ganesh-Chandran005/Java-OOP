35c5f45404941682efa605c5f3e59067ca8b50ef
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-08-01 09:49:28 +0530

    Add files via upload

---
+++ StudentCount.java
@@ -0,0 +1,26 @@
+package Test;
+class Student{
+	String name;
+	int rollNumber;
+	static int studentCount=0;
+	Student(String name,int rollNumber){
+		this.name=name;
+		this.rollNumber=rollNumber;
+		studentCount++;
+	}
+	public static void displayStudentCount() {
+		System.out.println("Name: " + name);
+		System.out.println("Roll Number: " + rollNumber);
+		System.out.println("Total Student count:" + studentCount);
+	}
+	
+}
+public class StudentCount {
+	public static void main(String[] args) {
+		Student student1 = new Student("Austin",14);
+		Student student2 = new Student("Bobby",26);
+		Student student3 = new Student("Ganesh",36);
+		Student.displayStudentCount();
+	}
+
+}
