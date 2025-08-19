0355dea996db28ae37d97d076ff4fdd7e8214057
GANESH CHANDRAN <ganeshchandran2028@cs.sjcetpalai.ac.in>
2025-08-19 14:32:35 +0530

    Add files via upload

---
+++ TwoDArray.java
@@ -0,0 +1,31 @@
+package Ganesh;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no of rows: ");
+		int row = sc.nextInt();
+		System.out.print("Enter the no of columns: ");
+		int col = sc.nextInt();
+		int [][] matrix = new int [row][col];
+		System.out.print("Enter matrix elements: ");
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				matrix[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print("Matrix: ");
+		System.out.println();
+		for(int i=0;i<row;i++) {
+			for(int j=0;j<col;j++) {
+				System.out.print(matrix[i][j] + "\t");
+				
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
